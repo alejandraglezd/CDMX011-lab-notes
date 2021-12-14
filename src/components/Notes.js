f0ae3225c578da06d325ea680c3b6d21db131213
alejandraglezd <alejandra.gnzlz.dz@gmail.com>
2021-12-14 14:22:52 -0600

    Implementa rutas

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -1,8 +1,21 @@
 import React from 'react';
+import './Notes.css';
 
 const Notes = () => (
-    <div>
-    Hola Notes
+    <div className="Notes">
+        <header className="App-header">
+          <h1>friendly <br></br>reminders.</h1>
+        </header>
+        <h3>A las palabras se las lleva el viento, qué bien que regresaste para vaciarlas aquí</h3>
+        <section className="note2">
+            <textarea className="note"></textarea>
+        </section>
+        <button className="addNote">Agregar nota</button>
+        {/* <button className="editNote">Editar</button>
+        <button className="deleteNote">Borrar</button> */}
+        <footer className="App-footer">
+            © 2021 friendly reminders.  x Alejandra Glez
+        </footer>
     </div>
 );
 export default Notes;
--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,8 +1,22 @@
 import React from 'react';
+import './SignIn.css';
 
-const SignIn = () => (
-    <div>
-    Hola SignIn
+function SignIn () {
+    return (
+    <div className="SignIn">
+      <header className="App-header">
+          <h1>friendly <br></br>reminders.</h1>
+        </header>
+        <h3>Mejor tarde que nunca ;)</h3>
+    <form className="homeForm">
+            <input className="inputEmail" placeholder="Correo electrónico"></input>
+            <input className="inputPassword" type="password" placeholder="Contraseña"></input>
+            <button className="buttonSignIn">Regístrate</button>
+          </form>
+          <footer className="App-footer">
+            © 2021 friendly reminders.  x Alejandra Glez
+        </footer>
     </div>
-);
+    );
+};
 export default SignIn;
